b29b6efafa6f7a8ac1eae7619ffb80b63cbe360f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2022-01-06 18:11:11 +0100

    binding to document instead of window

--- js/inject.js
+++ js/inject.js
@@ -1,15 +1,16 @@
 (function(d, clientScriptsBaseUrl, filterServiceBaseUrl) {
-  script = d.createElement('script');
+  let script = d.createElement('script');
   script.type = 'text/javascript';
   script.async = true;
   script.onload = function(){
     // idiomReplaceX.minWordThreshold = 2;
-    idiomReplaceX.filterServiceBaseUrl = filterServiceBaseUrl;
-    idiomReplaceX.ui(clientScriptsBaseUrl);
-    var debug = false;
-    idiomReplaceX.extractTextBlocks(debug);
-    idiomReplaceX.requestForReplaceX();
+    d.idiomReplaceX.filterServiceBaseUrl = filterServiceBaseUrl;
+    d.idiomReplaceX.ui(clientScriptsBaseUrl);
+    let debug = false;
+    d.idiomReplaceX.extractTextBlocks(debug);
+    d.idiomReplaceX.requestForReplaceX();
   };
   script.src = clientScriptsBaseUrl + 'js/idiomReplaceX.js';
   d.getElementsByTagName('head')[0].appendChild(script);
-}(document, 'https://bgbm14463lap/', 'https://bgbm14463lap/service/'));
+// }(document, 'https://bgbm14463lap/', 'https://bgbm14463lap/service/src/'));
+}(document, 'https://idiomreplacex.de/client/', 'https://idiomreplacex.de/service/src/'));
