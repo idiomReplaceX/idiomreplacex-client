62db8d899e796a80834802af70981d92318d0449
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2022-01-07 00:11:13 +0100

    reverting inject.js to last version

--- js/inject.js
+++ js/inject.js
@@ -4,11 +4,11 @@
   script.async = true;
   script.onload = function(){
     // idiomReplaceX.minWordThreshold = 2;
-    d.idiomReplaceX.filterServiceBaseUrl = filterServiceBaseUrl;
-    d.idiomReplaceX.ui(clientScriptsBaseUrl);
-    let debug = false;
-    d.idiomReplaceX.extractTextBlocks(debug);
-    d.idiomReplaceX.requestForReplaceX();
+    idiomReplaceX.filterServiceBaseUrl = filterServiceBaseUrl;
+    idiomReplaceX.ui(clientScriptsBaseUrl);
+    var debug = false;
+    idiomReplaceX.extractTextBlocks(debug);
+    idiomReplaceX.requestForReplaceX();
   };
   script.src = clientScriptsBaseUrl + 'js/idiomReplaceX.js';
   d.getElementsByTagName('head')[0].appendChild(script);
