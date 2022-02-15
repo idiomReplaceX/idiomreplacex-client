e6182c818919899c9bb9a78595af1f5478889e1d
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2022-02-15 13:56:26 +0100

    adding activate.js to replace inject.js

---
+++ js/activate.js
@@ -0,0 +1,9 @@
+let  clientScriptsBaseUrl = 'https://idiomreplacex.de/client/';
+let  filterServiceBaseUrl = 'https://idiomreplacex.de/service/src/';
+
+// idiomReplaceX.minWordThreshold = 2;
+idiomReplaceX.filterServiceBaseUrl = filterServiceBaseUrl;
+idiomReplaceX.ui(clientScriptsBaseUrl);
+let debug = false;
+idiomReplaceX.extractTextBlocks(debug);
+idiomReplaceX.requestForReplaceX();
